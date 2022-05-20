39f2b64f21e081b0715c9ca355f95909be587496
p2105285 <101558798+p2105285@users.noreply.github.com>
2022-05-20 16:40:22 +0200

    Add files via upload

---
+++ Exemple.java
@@ -0,0 +1,20 @@
+
+public class Exemple {
+
+	public static void main(String[] args) {
+		Warrior warrior = new Warrior ("Jean -Luc") ;
+		Mage mage = new Mage (" Robert ") ;
+		warrior . moveRight () ;
+		warrior . moveLeft () ;
+		warrior . moveBack () ;
+		warrior . moveForward () ;
+		mage . moveRight () ;
+		mage . moveLeft () ;
+		mage . moveBack () ;
+		mage . moveForward () ;
+
+
+
+	}
+
+}
